2ab3d0f4a6d8ecd26e5fd3d802266e0433cbc489
ishikawatatuya <shichuanzi83@gmail.com>
2020-12-05 16:12:19 +0900

    提出　勉強不足

--- app/src/main/java/com/example/check2/MainActivity.kt
+++ app/src/main/java/com/example/check2/MainActivity.kt
@@ -1,12 +1,43 @@
 package com.example.check2
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val count: Int = 0
+
+        button.setOnClickListener {
+            count++
+            countText.text = count.toString()
+        }
+
+        button2.setOnClickListener {
+            count--
+            countText.text = count.toString()
+        }
+
+        button3.setOnClickListener {
+            count+10
+            countText.text = count.toString()
+        }
+
+        button4.setOnClickListener {
+            count - 10
+            countText.text = count.toString()
+        }
+
+
+        button.setBackgroundColor(Color.parseColor("#47885e"))
+        button2.setBackgroundColor(Color.parseColor("#47885e"))
+        button3.setBackgroundColor(Color.parseColor("#47885e"))
+        button4.setBackgroundColor(Color.parseColor("#47885e"))
     }
 }
